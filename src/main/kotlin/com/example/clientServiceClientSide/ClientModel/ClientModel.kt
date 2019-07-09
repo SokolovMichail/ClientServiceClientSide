e9905c35ae75636ecb6a0a785094bbc79eebf139
SokolovMichail <sokolovmisha44@gmail.com>
2019-07-09 11:04:57 +0300

    Refactored ClientModel.Client, its second_name now secondName

--- src/main/kotlin/com/example/clientServiceClientSide/ClientModel/ClientModel.kt
+++ src/main/kotlin/com/example/clientServiceClientSide/ClientModel/ClientModel.kt
@@ -6,7 +6,7 @@ class Client(
         var id: Int? = 4,
         var surname: String = "Pass",
         var name: String = "Pass",
-        var second_name: String = "Pass",
+        var secondName: String = "Pass",
         var dr: Date = Date(),
         var account:String="ERR_NO_ACC_DET",
         var bd:String=Date().toString())
